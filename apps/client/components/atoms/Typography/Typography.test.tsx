63331f4197732e046b1b056e5857923e77b44ad8
navi9000 <icdi7434@gmail.com>
2026-02-20 17:16:44 +0300

    setup tests and workflow

---
+++ apps/client/components/atoms/Typography/Typography.test.tsx
@@ -0,0 +1,17 @@
+import { composeStories } from "@storybook/react"
+import { render } from "@testing-library/react"
+import * as stories from "./Typography.stories"
+import { describe, test, expect } from "vitest"
+
+const { Default } = composeStories(stories)
+
+describe("Typography", () => {
+  test("should show on the screen", async () => {
+    const { getByText } = render(<Default>Hello</Default>)
+
+    expect(getByText("Hello")).toBeInTheDocument()
+  })
+  test("basic test", () => {
+    expect(true).toBeTruthy()
+  })
+})
